7b0b8d08c2920723a24bf66b700c8c1c0c1bfee0
lekaixin <82202604+ALekevin@users.noreply.github.com>
2021-08-18 17:21:32 +0800

    Update test_wx_addlinkman.py

--- HomeWork/Selenium_Hw_19/Selenium_Hw_19_6_24/test_wx_addlinkman.py
+++ HomeWork/Selenium_Hw_19/Selenium_Hw_19_6_24/test_wx_addlinkman.py
@@ -39,6 +39,7 @@ class TestWxlinkname:
             chrome_options.add_argument('--disable-gpu')
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument('window-size=1920x3000')
 
         self.driver = webdriver.Chrome(executable_path=executable_path, options=chrome_options)
 
