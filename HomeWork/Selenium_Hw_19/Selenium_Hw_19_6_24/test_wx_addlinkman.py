4904d3ad6fa86d1d690e1bba4ef6a42afe7bc8d9
lekaixin <82202604+ALekevin@users.noreply.github.com>
2021-08-18 22:36:28 +0800

    Update test_wx_addlinkman.py

--- HomeWork/Selenium_Hw_19/Selenium_Hw_19_6_24/test_wx_addlinkman.py
+++ HomeWork/Selenium_Hw_19/Selenium_Hw_19_6_24/test_wx_addlinkman.py
@@ -36,10 +36,10 @@ class TestWxlinkname:
         if using_headless is not None and using_headless.lower() == 'true':
             print('使用无界面方式运行')
             chrome_options.add_argument("--headless")
-            chrome_options.add_argument('--disable-gpu')
-            chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument("--no-sandbox")
-            chrome_options.add_argument('window-size=1920x3000')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument('window-size=1920x3000')
 
         self.driver = webdriver.Chrome(executable_path=executable_path, options=chrome_options)
 
